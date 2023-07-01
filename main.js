71a03b2ad50bc7b32f26a95c1ec827d99eb54934
jaiyelith <jaii_b@hotmail.com>
2023-07-01 18:53:28 -0500

    se añadieron nuevos datos y funciones a la lista

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
-let nombresContactos = ["Giovanny Hernandez" , "Jaime Gonzalez" , "Dailibeth Lugo" , "Jesus Fuentes"];
+let nombresContactos = [];
 
-/*Funcion para agregar un nuevo contacto a la lista*/
-function agregarNuevoContacto(contacto){
+//Función para añadir un nuevo contacto a una lista
+function agregarNuevoContacto(nombre, apellido, telf, ciudad, direccion){
+    const contacto = {
+        id: nombresContactos.length + 1,
+        nombre,
+        apellido,
+        telefono: telf,
+        ubicaciones: {
+            ciudad,
+            direccion
+        }
+    }
     return nombresContactos.push(contacto);
 }
 
-/* agregarNuevoContacto("Graciela Zambrano");
-console.log(agregarNuevoContacto); */
+agregarNuevoContacto("Giovanny", "Hernandez", "3017765480", "Bogotá", "Portal de Madelena");
+agregarNuevoContacto("Jaime", "Gonzalez", "3028896215", "Bucaramanga", "Madelena Urbano 2");
+agregarNuevoContacto("Dailibeth","Lugo", "3008829687", "Trujillo", "La Rinconada");
+agregarNuevoContacto("Jesus", "Fuentes", "3037946213", "San Francisco", "La Noria");
+console.log(nombresContactos);
 
-/* Funcion para eliminar un contacto de la lista*/
-function suprimirContacto(contacto){
+//Función para borrar un contacto existente de la lista
+function suprimirContacto(nombreContacto){
+    const nuevoContacto = nombreContacto.split(" ");
+    const nombre = nuevoContacto[0];
+    const apellido = nuevoContacto[1];
     for(let i = 0; i < nombresContactos.length; i++){
-        if(nombresContactos[i] == contacto){
+        if(nombresContactos[i].nombre === nombre && nombresContactos[i].apellido === apellido){
             return nombresContactos.splice(i, 1);
         }
     }
 }
 
-/*suprimirContacto("Jaime Gonzalez");
-console.log(nombresContactos);*/
+suprimirContacto("Jaime Gonzalez");
+suprimirContacto("Jesus Fuentes");
+console.log(nombresContactos);
 
-
-/* Funcion para imprimir la lista de contactos*/
+//Función para imprimir en consola los contactos presentes en la lista
 function imprimirContacto(){
     for (const contacto of nombresContactos) {
         console.log(contacto);
     }
 }
 
-/*imprimirContacto();
-console.log(nombresContactos);*/
+imprimirContacto();
